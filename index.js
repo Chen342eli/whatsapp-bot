715fada30dc9a8c142b101693df7ce960c81720d
chen342know <chen342know@gmail.com>
2026-04-30 12:20:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-const OpenAI = require("openai");
 const express = require("express");
 const app = express();
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/webhook", async (req, res) => {
+app.post("/webhook", (req, res) => {
   const incomingMsg = req.body.Body;
 
-  console.log("Message:", incomingMsg);
-
-const completion = await openai.chat.completions.create({
-  model: "gpt-4o-mini",
-  messages: [
-    {
-      role: "system",
-      content: "אתה באדי, מאמן תזונתי אישי תומך ומעודד"
-    },
-    {
-      role: "user",
-      content: incomingMsg
-    }
-  ],
-});
-
-const reply = completion.choices[0].message.content;
-
-const response = `
-  <Response>
-    <Message>${reply}</Message>
-  </Response>
-`;
+  const response = `
+    <Response>
+      <Message>קיבלתי: ${incomingMsg}</Message>
+    </Response>
+  `;
 
   res.set("Content-Type", "text/xml");
   res.send(response);
@@ -41,5 +18,5 @@ const response = `
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running");
 });
