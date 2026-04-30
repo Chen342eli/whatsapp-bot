b7ebf90bc39542d64d4af2626d6413be6047977a
Chen342eli <chen342eli@gmail.com>
2026-04-30 11:09:05 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/webhook", (req, res) => {
+  const incomingMsg = req.body.Body;
+
+  console.log("Message:", incomingMsg);
+
+  const response = `
+    <Response>
+      <Message>קיבלתי: ${incomingMsg}</Message>
+    </Response>
+  `;
+
+  res.set("Content-Type", "text/xml");
+  res.send(response);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
